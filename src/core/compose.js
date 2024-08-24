edbf210751a8d961decd52112fec315a547b4347
michael <michael@shoshi.dog>
2024-08-24 04:45:52 +0300

    add typings

--- src/core/compose.js
+++ src/core/compose.js
@@ -1,19 +1,34 @@
 import Fail from '../util/fail.js'
 
+/** @typedef { import('core.js').IRequest } Req */
+/** @typedef { import('core.js').IResponse } Res */
+/** @typedef { import('core.js').Handler } Handler */
+/**
+* @param { Handler[] } args
+* @return { Handler }
+*/
 export default function compose(args) {
-
-    return function handler(...a) {
+    /**
+    * @param { Req } rq
+    * @param { Res } rs
+    * @return { Promise<any> }
+    */
+    return function handler(rq, rs) {
         let prev = -1
 
+        /**
+         * @param { number  } i
+         * @return { Promise<any> }
+         */
         const emit = i => {
             if (i <= prev || i > args.length)
-                return Fail.deny('next called multiple times', 500)
+                return Fail.deny('next called multiple times', { cause: 500 })
 
             const cb = args[ prev = i ]
 
             try {
                 return Promise.resolve(cb
-                    ? cb.apply(this, a.concat(() => emit(i + 1)))
+                    ? cb.call(this, rq, rs, () => emit(i + 1))
                     : this)
             }
             catch (cause) {
